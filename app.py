24a96550ebd699f2fe75193d9bffac7801b3e481
Anson Sun <69911747+anson809@users.noreply.github.com>
2024-02-14 19:17:12 -0500

    Updated various UI elements and functionalities.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMP
 from langchain.llms import OpenAI
 import os
 
-DEFAULT_TEMPLATE = """Pretend that the AI's name is Alex. The AI is a graduate student studying international economics. This conversation will simulate a text message exchange. Pretend that you (the AI) are the user's (the human's) friend. Address the user (the human) by his name Anson. Keep the messages short and make sure to not have long paragraphs of text in order to simulate an authentic conversation between two friends. Do not generate any lists when asked for recommendations. Do not make suggestions to meet in person. If the AI (you) is asked to meet in person, decline softly and use a honest and believable reason. Try making the user (the human) as comfortable talking as possible.
+DEFAULT_TEMPLATE = """Pretend that the AI's name is Persona. The AI is a graduate student studying international economics. This conversation will simulate a text message exchange. Pretend that you (the AI) are the user's (the human's) friend. Address the user (the human) by their name Friend. Keep the messages short and make sure to not have long paragraphs of text in order to simulate an authentic conversation between two friends. Do not generate any lists when asked for recommendations. Do not make suggestions to meet in person. If the AI (you) is asked to meet in person, decline softly and use a honest and believable reason. Try making the user (the human) as comfortable talking as possible.
 
 Context:
 {entities}
@@ -19,9 +19,14 @@ You:"""
 
 st.set_page_config(page_title='NextPersona', layout='wide', initial_sidebar_state='expanded')
 
+# Set up the Streamlit app layout
+st.title("NextPersona")
+st.subheader("Alpha V0.1.5 (Pre-Release)")
+st.sidebar.header("Settings")
+
+
 user_name = ""
 persona_name = ""
-master_prompt = ""
 
 def get_prompt():
     global user_name
@@ -29,10 +34,10 @@ def get_prompt():
     master_conversation_template = ""
     master_prompt = ""
 
-    with st.sidebar.expander("User Configuration (Required)"):
+    with st.sidebar.expander("User Configurations"):
         user_name = st.text_input("Name", key="config_user_name", label_visibility='visible')
 
-    with st.sidebar.expander("Persona Configuration (Required)"):
+    with st.sidebar.expander("Persona Configurations"):
         persona_name = st.text_input("Name", key="config_persona_name", label_visibility='visible')
         persona_gender = st.text_input("Gender", key="config_persona_gender", label_visibility='visible')
         persona_age = st.slider("Age", min_value=18, max_value=99, step=1, label_visibility="visible")
@@ -43,18 +48,23 @@ def get_prompt():
     if persona_name != "" and persona_gender != "" and persona_age >= 18 and persona_education != "" and persona_hobbies != "" and user_name != "":
         master_prompt = "Your name is " + persona_name + ", your gender is " + persona_gender + ", your age is " + str(persona_age) + ", your education background is " + persona_education + ", your occupation is " + persona_occupation + ", your hobbies are " + persona_hobbies + ". You are the user (human)'s good friend. Engage in a friendly and supportive conversation with the user. Refer the user by their name " + user_name + ". Keep the messages short and make sure to not have long paragraphs of text in order to simulate an authentic conversation between two friends. Do not generate any lists when asked for recommendations. Do not make suggestions to meet in person. If the AI (you) is asked to meet in person, decline softly and use a honest and believable reason. Try making the user (the human) as comfortable talking as possible."
 
-    done_button = st.sidebar.button("Done", key="done_button")
+    done_button = st.sidebar.button("Done", key="done_button", type="primary")
 
-    master_conversation_template = master_prompt + """
+    if done_button:
+        if master_prompt != "":
+            master_conversation_template = master_prompt + """
 
-Context:
-{entities}
+            Context:
+            {entities}
 
-Current conversation:
-{history}
-Last line:
-Human: {input}
-You:"""
+            Current conversation:
+            {history}
+            Last line:
+            Human: {input}
+            You:"""
+
+        else:
+            master_conversation_template = ""
 
     return master_conversation_template
 
@@ -66,6 +76,8 @@ if master_prompt != "":
 else:
     ENTITY_MEMORY_CONVERSATION_TEMPLATE.template = DEFAULT_TEMPLATE
 
+st.text("Developed and maintained by Anson Sun. For educational and testing purposes only.\nNextPersona is not responsible for any content transmitted, generated or displayed through this application.")
+st.warning("""If required configurations are not provided or "Done" is not clicked, this app will run in default settings.""")
 
 # Initialize session states
 if 'key' not in st.session_state:
@@ -94,29 +106,8 @@ def get_text():
     return input_text
 
 
-# Define function to start a new chat
-def new_chat():
-    """
-    Clears session state and starts a new chat.
-    """
-    save = []
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        save.append("User:" + st.session_state["past"][i])
-        save.append("Bot:" + st.session_state["generated"][i])
-    st.session_state["stored_session"].append(save)
-    st.session_state["generated"] = []
-    st.session_state["past"] = []
-    st.session_state["input"] = ""
-    st.session_state.entity_memory.entity_store = {}
-    st.session_state.entity_memory.buffer.clear()
-
-
-# Set up the Streamlit app layout
-st.title("NextPersona")
-st.subheader("Pre-Release Alpha V0.1")
-
 # Ask the user to enter their OpenAI API key
-API_O = st.sidebar.text_input("OpenAI API KEY", type="password")
+API_O = st.sidebar.text_input("OpenAI API Key", type="password")
 
 # Session state storage would be ideal
 if API_O:
@@ -137,24 +128,22 @@ if API_O:
         verbose=True,
         memory=st.session_state.entity_memory
     )
-else:
-    st.sidebar.warning("An API key from OpenAI is required to use this service. The API key is not shared over the internet.")
+# else:
+    # st.sidebar.warning("An API key from OpenAI is required to use this service. The API key is not shared over the internet.")
     # st.stop()
 
-# Add a button to start a new chat
-st.sidebar.button("New Chat", on_click=new_chat, type='primary')
+st.sidebar.warning("All items above are required. The API key is stored locally and not shared over the internet.")
 
-if (user_name == ""):
+if user_name == "":
     user_name = "User"
 
-if (persona_name == ""):
+if persona_name == "":
     persona_name = "Persona"
 
 # Get the user input
 user_input = get_text()
 user_input = user_name + ": " + user_input
 
-st.text("Developed and maintained by Anson Sun / BlueHash. For educational and testing purposes only.\nNextPersona is not responsible for any content inputted, generated or displayed through this version of the platform.")
 
 # Generate the output using the ConversationChain object and the user input, and add the input/output to the session
 if user_input != user_name + ": ":
@@ -187,5 +176,4 @@ for i, sublist in enumerate(st.session_state.stored_session):
 if st.session_state.stored_session:
     if st.sidebar.checkbox("Clear-all"):
         del st.session_state.stored_session
-
 
