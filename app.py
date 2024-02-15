890186a7889c33f24b356ba482c3d7cb4cfe0f69
Anson Sun <69911747+anson809@users.noreply.github.com>
2024-02-15 00:47:16 -0500

    Implemented UI improvements.

--- app.py
+++ app.py
@@ -28,8 +28,6 @@ st.sidebar.header("Settings")
 user_name = ""
 persona_name = ""
 
-# Ask the user to enter their OpenAI API key
-API_O = st.sidebar.text_input("OpenAI API Key", type="password")
 
 def get_prompt():
     global user_name
@@ -51,23 +49,23 @@ def get_prompt():
     if persona_name != "" and persona_gender != "" and persona_age >= 18 and persona_education != "" and persona_hobbies != "" and user_name != "":
         master_prompt = "Your name is " + persona_name + ", your gender is " + persona_gender + ", your age is " + str(persona_age) + ", your education background is " + persona_education + ", your occupation is " + persona_occupation + ", your hobbies are " + persona_hobbies + ". You are the user (human)'s good friend. Engage in a friendly and supportive conversation with the user. Refer the user by their name " + user_name + ". Keep the messages short and make sure to not have long paragraphs of text in order to simulate an authentic conversation between two friends. Do not generate any lists when asked for recommendations. Do not make suggestions to meet in person. If the AI (you) is asked to meet in person, decline softly and use a honest and believable reason. Try making the user (the human) as comfortable talking as possible."
 
-    done_button = st.sidebar.button("Done", key="done_button", type="primary")
+    # done_button = st.sidebar.button("Done", key="done_button", type="primary")
 
-    if done_button:
-        if master_prompt != "":
-            master_conversation_template = master_prompt + """
+    # if done_button:
+    if master_prompt != "":
+        master_conversation_template = master_prompt + """
 
-            Context:
-            {entities}
+        Context:
+        {entities}
 
-            Current conversation:
-            {history}
-            Last line:
-            Human: {input}
-            You:"""
+        Current conversation:
+        {history}
+        Last line:
+        Human: {input}
+        You:"""
 
-        else:
-            master_conversation_template = ""
+    else:
+        master_conversation_template = ""
 
     return master_conversation_template
 
@@ -80,7 +78,7 @@ else:
     ENTITY_MEMORY_CONVERSATION_TEMPLATE.template = DEFAULT_TEMPLATE
 
 st.text("Developed and maintained by Anson Sun. For educational and testing purposes only.\nNextPersona is not responsible for any content transmitted, generated or displayed through this application.")
-st.warning("""If required configurations are not provided or "Done" is not clicked, this app will run with default settings.""")
+st.warning("""Please use configuration settings to personalize your unique persona. This app will run with default settings if no configuration parameter is provided.""")
 
 # Initialize session states
 if 'key' not in st.session_state:
@@ -109,6 +107,10 @@ def get_text():
     return input_text
 
 
+# Ask the user to enter their OpenAI API key
+API_O = st.sidebar.text_input("OpenAI API Key", type="password")
+
+
 # Session state storage would be ideal
 if API_O:
     # Create an OpenAI instance
@@ -128,11 +130,11 @@ if API_O:
         verbose=True,
         memory=st.session_state.entity_memory
     )
-# else:
-    # st.sidebar.warning("An API key from OpenAI is required to use this service. The API key is not shared over the internet.")
+else:
+    st.sidebar.warning("An API key from OpenAI is required to use this application. The API key remains private and is not publicly accessible.")
     # st.stop()
 
-st.sidebar.warning("All items above are required. The API key is stored locally and is not shared over the internet.")
+# st.sidebar.warning("All items above are required. The API key is stored locally and is not shared over the internet.")
 
 if user_name == "":
     user_name = "User"
@@ -176,4 +178,5 @@ for i, sublist in enumerate(st.session_state.stored_session):
 if st.session_state.stored_session:
     if st.sidebar.checkbox("Clear-all"):
         del st.session_state.stored_session
+
 
